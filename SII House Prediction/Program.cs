1f86f3228e377d94ae5ca7f728281d39a275574f
tedi_ <tedi_@TEDI-LAPTOP>
2024-10-29 23:05:57 +0200

    Add project files.

---
+++ SII House Prediction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SII_House_Prediction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Load sample data
+            var sampleData = new MLModel1.ModelInput()
+            {
+                Longitude = -122.22F,
+                Latitude = 37.86F,
+                Housing_median_age = 21F,
+                Total_rooms = 7099F,
+                Total_bedrooms = 1106F,
+                Population = 2401F,
+                Households = 1138F,
+                Median_income = 8.3014F,
+                Ocean_proximity = @"NEAR BAY",
+            };
+
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            Console.WriteLine($"Pret: {result.Score}");
+            Console.ReadLine();
+        }
+    }
+}
